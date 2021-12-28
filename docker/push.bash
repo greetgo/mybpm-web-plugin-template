f726267404dd36b4ef11854990b677e267853c09
mkolpakov <mkolpakov@greet-go.com>
2021-12-28 15:53:51 +0600

    fix: поправил bash

--- docker/push.bash
+++ docker/push.bash
@@ -5,7 +5,7 @@ cd "$(dirname "$0")" || exit 113
 IMAGE_NAME="$(bash ./meta/name.bash)"
 VERSION="$(bash ./meta/version.bash)"
 
-bash ./image.sh
+bash ./image.bash
 
 docker push "${IMAGE_NAME}:${VERSION}"
 echo PUSHED IMAGE "${IMAGE_NAME}:${VERSION}"
