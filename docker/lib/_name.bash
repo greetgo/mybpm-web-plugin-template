1bd744c105bcf86c593b9d1769e607b3e9ae85a8
mkolpakov <mkolpakov@greet-go.com>
2021-12-29 10:35:39 +0600

    corrected name

---
+++ docker/lib/_name.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+###MODIFY replace template {PLUGIN_NAME.under}
+echo -n dockerhub.mybpm.kz/mybpm-web-with-template
--- docker/meta/name.bash
+++ docker/meta/name.bash
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")" || exit 113
 
-IMAGE_NAME=$(cat ../lib/name.txt);
+IMAGE_NAME=$(bash ../lib/_name.bash);
 BRANCH="$(bash branch.bash)"
 
 echo "${IMAGE_NAME}-${BRANCH}" | awk '{print tolower($0)}'
